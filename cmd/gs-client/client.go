c3f99cbcccdb4b76afebbcd3d6bda4ab9a1d0126
julien-beguier <mani.crim@gmail.com>
2021-06-24 16:56:38 +0200

    Improved network communication, reads until EOT

--- cmd/gs-client/client.go
+++ cmd/gs-client/client.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "os"
-    "bufio"
-//    "strings"
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
 )
 
 const (
     CONN_TYPE = "tcp"
+    EOT = 4
+)
+
+const (
+    ACK = "ACK"
+    NACK = "NACK"
+    DISCONNECT = "DISCONNECT"
 )
 
 func main() {
@@ -22,13 +29,14 @@ func main() {
     serverAddr := pArgs[0]
     serverPort := pArgs[1]
 
-    // Initiating connection
+    // Resolving address
     fmt.Printf("Connecting to %s:%s\n", serverAddr, serverPort)
     tcpAddr, err := net.ResolveTCPAddr(CONN_TYPE, serverAddr + ":" + serverPort)
     if err != nil {
         println("ResolveTCPAddr failed:", err.Error())
         os.Exit(1)
     }
+    // Initiating connection
     conn, err := net.DialTCP(CONN_TYPE, nil, tcpAddr)
     if err != nil {
         println("Dial failed:", err.Error())
@@ -38,35 +46,69 @@ func main() {
     // Creating reader to read user input
     reader := bufio.NewReader(os.Stdin)
     for {
-        line, _ := reader.ReadString('\n')
-        _, err = conn.Write([]byte(line))
+        rawLine, _ := reader.ReadString('\n')
+        writeData := normalizeWriteData(rawLine)
+        _, err = conn.Write(writeData)
         if err != nil {
             println("Write to server failed:", err.Error())
             os.Exit(1)
         }
 
-    // TODO
-    // Read server response
-/*
-        // Read data line by line
-        netData, err := bufio.NewReader(conn).ReadString('\n')
+        // Read server response
+        readData, err := bufio.NewReader(conn).ReadString(EOT)
         if err != nil {
             fmt.Println("Error: ", err.Error())
             return
         }
 
         // Print message received
-        data := string(netData)
-        fmt.Println(">> %s", data)
+        reply := normalizeReadData(readData)
+        fmt.Printf(">> %s\n", reply)
 
-        // Interpret data
-        reply := strings.TrimSpace(data)
-        if reply == "DISCONNECT" {
-            fmt.Println("Received disconnect command, exiting ...")
+        // Handle disconnect if a "DISCONNECT" is received
+        if reply == DISCONNECT + ":" + ACK {
+            fmt.Println("Received disconnect acknowledge, closing connection ...")
             break
         }
-*/
+
+        // Interpret data
     }
     conn.Close()
-    fmt.Println("Connection to %s:%s closed", serverAddr, serverPort)
+    fmt.Printf("Connection to %s:%s closed", serverAddr, serverPort)
+}
+
+func normalizeWriteData(writeData string) []byte {
+    // Build the line with EOT as the last byte
+
+    // Removes '\n'
+    res := strings.ReplaceAll(writeData, "\n", "")
+
+    // Init a slice with len + 1 for EOT
+    slice := make([]byte, 0, len(res) + 1)
+
+    // Append line into slice
+    slice = append(slice, res...)
+
+    // Add EOT at the end of slice
+    slice = append(slice, EOT)
+
+    return slice
+}
+
+func normalizeReadData(readData string) string {
+    // Build the read data without EOT as the last byte
+
+    // Get the lenght of the read data
+    lenData := len(readData)
+
+    // Init a slice with juste enough space for the actual data whithout any '\n' or EOT
+    slice := make([]byte, 0, lenData)
+
+    // Append the data with EOT
+    slice = append(slice, readData...)
+
+    // Remove the last byte (EOT)
+    slice = append(slice[:lenData - 1], slice[lenData:]...)
+
+    return string(slice)
 }
--- cmd/gs-server/server.go
+++ cmd/gs-server/server.go
@@ -1,15 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "os"
-    "bufio"
-    "strings"
+	"bufio"
+	"fmt"
+	"net"
+	"os"
 )
 
 const (
     CONN_TYPE = "tcp"
+    EOT = 4
+)
+
+const (
+    ACK = "ACK"
+    NACK = "NACK"
+    DISCONNECT = "DISCONNECT"
 )
 
 func main() {
@@ -21,6 +27,10 @@ func main() {
     }
     listenPort := pArgs[0]
 
+    startListen(listenPort)
+}
+
+func startListen(listenPort string) {
     // Listen for incoming connections
     l, err := net.Listen(CONN_TYPE, ":" + listenPort)
     if err != nil {
@@ -39,29 +49,86 @@ func main() {
             fmt.Println("Error accepting: ", err.Error())
             os.Exit(1)
         }
+
+        // Add conn to a map containing all connected clients
+
         // Handle connections in a new goroutine
-        go handleRequest(conn)
+        go handleConnection(conn)
     }
 }
 
-func handleRequest(conn net.Conn) {
+func handleConnection(conn net.Conn) {
     // Loop for each new connection
     for {
         // Read data line by line
-        netData, err := bufio.NewReader(conn).ReadString('\n')
+        readData, err := bufio.NewReader(conn).ReadString(EOT)
         if err != nil {
             fmt.Println("Error: ", err.Error())
             return
         }
 
         // Print & interpret data
-        data := strings.TrimSpace(string(netData))
+        data := normalizeReadData(readData)
         fmt.Printf(">> %s\n", data)
-        if data == "DISCONNECT" {
-            // Close connection if loop ends
+
+        // Handle disconnect if a "DISCONNECT" is received
+        if data == DISCONNECT {
             fmt.Println("Received a disconnect, closing connection ...")
-            conn.Close()
+            respondClient(conn, DISCONNECT + ":" + ACK)
             break
         }
+        interpretData(conn, data)
     }
+    // Close connection when out of the loop
+    conn.Close()
+}
+
+func interpretData(conn net.Conn, data string) {
+        if data == "ping" {
+            respondClient(conn, "pong")
+        } else {
+            respondClient(conn, data)
+        }
+}
+
+func respondClient(conn net.Conn, data string) {
+    // Build the response with EOT as the last byte
+    slice := normalizeWriteData(data)
+
+    // Send response to client
+    conn.Write(slice)
+    fmt.Printf("<< %s\n", data)
+}
+
+func normalizeWriteData(writeData string) []byte {
+    // Build the write data with EOT as the last byte
+
+    // Init a slice with len + 1 for EOT
+    slice := make([]byte, 0, len(writeData) + 1)
+
+    // Append writeData into slice
+    slice = append(slice, writeData...)
+
+    // Add EOT at the end of slice
+    slice = append(slice, EOT)
+
+    return slice
+}
+
+func normalizeReadData(readData string) string {
+    // Build the read data without EOT as the last byte
+
+    // Get the lenght of the read data
+    lenData := len(readData)
+
+    // Init a slice with juste enough space for the actual data whithout any '\n' or EOT
+    slice := make([]byte, 0, lenData)
+
+    // Append the data with EOT
+    slice = append(slice, readData...)
+
+    // Remove the last byte (EOT)
+    slice = append(slice[:lenData - 1], slice[lenData:]...)
+
+    return string(slice)
 }
